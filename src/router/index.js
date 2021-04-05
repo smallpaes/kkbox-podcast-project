c53adf2dd65e29107ed1881ba2e9d51712b6d021
Mike <mike.huang.mikank@gmail.com>
2021-04-05 17:16:17 +0800

    Add not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter);
 
@@ -18,10 +18,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
-];
+  {
+    path: '*',
+    name: 'NotFound',
+    component: () => import('@/views/NotFound')
+  }
+]
 
 const router = new VueRouter({
   routes,
-});
+})
 
-export default router;
+export default router
---
+++ src/views/NotFound.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="not-found">
+    <font-awesome-icon
+      class="not-found__icon"
+      icon="broadcast-tower"
+    />
+    <h1 class="not-found__description">
+      Page Not Found
+    </h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+
+<style lang="scss" scoped>
+.not-found {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &__icon {
+    color: $theme-text-color-primary;
+    font-size: $theme-font-size-x-jumbo;
+  }
+
+  &__description {
+    margin-top: 10px;
+    font-size: $theme-font-size-jumbo;
+  }
+}
+</style>

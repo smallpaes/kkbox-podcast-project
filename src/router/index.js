4fbc7502ee322575bfb5dc9238a48af2366a0f27
Mike <mike.huang.mikank@gmail.com>
2021-04-05 17:42:28 +0800

    Add episode page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeLayout from '../components/layout/HomeLayout.vue'
 import Home from '../views/home/Index.vue'
+import Episode from '../views/episode/Index.vue'
 
 Vue.use(VueRouter);
 
@@ -14,7 +15,12 @@ const routes = [
         path: '/',
         name: 'Home',
         component: Home
-      }
+      },
+      {
+        path: '/episode/:guid',
+        name: 'Episode',
+        component: Episode
+      },
     ]
   },
   {
---
+++ src/views/episode/Index.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="container episode">
+    <potcast-profile 
+      :profile-data="{
+        image: episodeData.itunes.image,
+        imageAlt: 'Episode Profile Picture',
+        title: episodeData.title
+      }"
+    >
+      <template #actions>
+        <button
+          type="button"
+          role="button"
+          class="profile__play-button btn--primary btn--large"
+          @click="playEpisode"
+        >
+          Play
+        </button>
+      </template>
+    </potcast-profile>
+    <div class="description">
+      <h1 class="description__title">
+        Episode Description
+      </h1>
+      <p class="description__content">
+        {{ episodeData.content }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+import PotcastProfile from '@/components/profile/PotcastProfile'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Episode',
+  components: {
+    PotcastProfile
+  },
+  data () {
+    return {
+      
+    }
+  },
+  computed: {
+    ...mapState('channel', ['episodes']),
+    episodeData () {
+      return this.episodes.find(episode => episode.guid === this.$route.params.guid)
+    }
+  },
+  created () {
+    
+  },
+  methods: {
+    async playEpisode () {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.description {
+  margin-top: 25px;
+  
+  &__content {
+    margin-top: 14px;
+    white-space: pre-wrap;
+  }
+}
+</style>

014c0d4e36846e555586e1a4d8ae9bbb889c569e
Mike <mike.huang.mikank@gmail.com>
2021-04-05 17:09:28 +0800

    Init project with dependencies and scss setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// Use library to specify what icons to be bundled eventually
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlay, faPause, faSpinner, faBroadcastTower } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlay, faPause, faSpinner, faBroadcastTower)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false;
 
